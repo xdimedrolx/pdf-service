40f921152c738cf48c02b2ea9d89d59605891a55
xdimedrolx <xdimedrolx@yandex.ru>
2022-09-01 12:24:49 +0300

    feat: add emulateMediaType option; bump puppeteer

--- server/src/resources/generator/generator.controller.js
+++ server/src/resources/generator/generator.controller.js
@@ -16,9 +16,19 @@ module.exports.generatePdf = async (ctx) => {
 
   const { url, html, options, headers } = result.value;
 
+  const opts = Object.assign({
+    printBackground: true,
+    margin: {
+      top: '0.4in',
+      right: '0.4in',
+      bottom: '0.4in',
+      left: '0.4in',
+    },
+  }, options);
+
   logger.debug('URL: ', url);
   logger.debug('HTML: ', html);
-  logger.debug('OPTIONS: ', options);
+  logger.debug('OPTIONS: ', opts);
   logger.debug('HEADERS: ', headers);
 
   const browser = await getBrowser();
@@ -33,20 +43,14 @@ module.exports.generatePdf = async (ctx) => {
     if (html) {
       await page.setContent(html);
     }
-    
+
+    if (typeof opts.emulateMediaType !== 'undefined' && opts.emulateMediaType) {
+      await page.emulateMediaType(opts.emulateMediaType);
+    }
+
     logger.debug('MAKE PDF');
 
-    ctx.body = await page.pdf(
-      Object.assign({
-        printBackground: true,
-        margin: {
-          top: '0.4in',
-          right: '0.4in',
-          bottom: '0.4in',
-          left: '0.4in',
-        },
-      }, options),
-    );
+    ctx.body = await page.pdf(opts);
     await page.close();
   } catch (e) {
     await closeBrowser(browser);
